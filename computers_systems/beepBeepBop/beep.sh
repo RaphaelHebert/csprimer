e9bf65e9c3f5a63cccf15071da99bc1d0195d86d
raphael-corize <raphael.hebert@corize.co.jp>
2026-07-19 11:11:58 +0200

    computer_systems(beepBeepBop/sh): make terminal beep - 2

--- computers_systems/beepBeepBop/beep.sh
+++ computers_systems/beepBeepBop/beep.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-for i in $(seq 1  $1);
-do
-    echo -ne '\007' 
+#!/bin/bash
+
+echo "Press keys (q to quit)..."
+
+while true; do
+    read -rsn1 key
+
+    case "$key" in
+        q)
+            echo "Quitting."
+            break
+            ;;
+        0)
+            ;;
+        [1-9])
+            for i in $(seq 1 $key);
+                do
+                    echo $i
+                    echo -ne '\007' 
+                    sleep 1
+                done
+            ;;
+        *)
+            echo "Press a number"
+            ;;
+    esac
 done

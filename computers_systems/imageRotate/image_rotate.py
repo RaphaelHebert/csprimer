df0183d3d30452dc4f0712afef87a302506801f6
raphael-corize <raphael.hebert@corize.co.jp>
2026-07-19 17:11:42 +0200

    computer_systems(imageRotate/py): setup project

---
+++ computers_systems/imageRotate/image_rotate.py
@@ -0,0 +1,6 @@
+# read the file as binary (hexdump)
+# extract and interpret the header(s)
+# extract the pixels
+# rotate the pixel's bytes 90 degres (using matrix operation?)
+# write a new the file (headers pixels etc) with the rotated image
+# test the solution
